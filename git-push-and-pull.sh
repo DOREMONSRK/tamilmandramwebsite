d1fcd8c6734b09e106c56faae6a26a2a791e9b29
DOREMONSRK <sanjayraamkuamr.1905119@srec.ac.in>
2023-03-15 18:22:54 +0530

    tamilmandram

--- git-push-and-pull.sh
+++ git-push-and-pull.sh
@@ -2,9 +2,13 @@ cd /c/Users/abishek\ veda/Desktop/tamilmandramwebsite
 
 while :
 do
+cd /c/Users/abishek\ veda/Desktop/tamilmandramwebsite
+
 ./git-pull.sh ;
 
 sleep 2;
+
+/c/Users/abishek veda/Desktop
 
 ./git-push.sh
 done
